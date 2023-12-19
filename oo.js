9d1ed072629cedb41492af50972c8ebbdb03edfb
Danielzanch <127164005+Danielzanch@users.noreply.github.com>
2023-12-19 20:20:02 -0300

    adicionando classe filha

--- oo.js
+++ oo.js
@@ -1,19 +1,32 @@
-function Empresa(nomeFuncionario, cargo, anoEntrada, anoSaida, salario) {
-    this.nomeFuncionario = nomeFuncionario;
-    this.cargo = cargo;
-    this.anoEntrada = anoEntrada;
-    this.anoSaida = anoSaida;
-    this.salario = salario;
+function Pessoa(nome, idade, ondeNasceu, Profisao) {
+    this.nome = nome;
+    this.idade = idade;
+    this.ondeNasceu = ondeNasceu;
+    this.Profisao = Profisao;
+
+    this.dizPosicao = function() {
+        console.log(this.posicaoJogador);
+    }
+
 }
 
-const funcDaniel = new Empresa("Daniel", "vistoriador/digitador", "2020", "2023", "2000,00");
-const funcKelvin = new Empresa("Kelvin", "vistoriador", "2021", "2023", "2000,00");
-const funcYuri = new Empresa("Yuri", "estagiario", "2023", "2023", "1500,00");
+function Jogador (nome, nomeProfissional, posicaoJogador, salarioJogador) {
+    Pessoa.call (this, nome);
+    this.posicaoJogador = posicaoJogador;
+    this.nomeProfissional = nomeProfissional;
+    this.salarioJogador = salarioJogador;
+}
 
-console.log(funcDaniel);
-console.log(funcKelvin);
-console.log(funcYuri);
+const Eduardo_Pereira = new Pessoa("Eduardo Pereira Rodrigues", "31 anos", "Goiânia", "Jogador de futebol");
+const Gustavo_Raúl = new Pessoa("Gustavo Raúl Gómez Portillo", "30 anos", "San Juan Bautista", "Jogador de futebol");
+const Raphael_Cavalcante = new Pessoa("Raphael Cavalcante Veiga", "28 anos", "São Paulo", "Jogador de futebol");
 
-console.log(funcDaniel instanceof Empresa)
-console.log(funcKelvin instanceof Empresa)
-console.log(funcYuri instanceof Empresa)
+const Dudu = new Jogador("Eduardo Pereira Rodrigues", "Dudu", "Meio-campista", "2,1 milhões por mês");
+const Gustavo_Gomes = new Jogador("Gustavo Raúl Gómez Portillo", "Gustavo Gomes", "Zagueiro", "1,1 milhão por mês");
+const Rafael_Veiga = new Jogador("Raphael Cavalcante Veiga", "Rafael Veiga", "Meio-Atacante", "1,0 milhão por mês");
+
+Dudu.dizPosicao();
+
+// console.log(Dudu);
+// console.log(Gustavo_Gomes);
+// console.log(Rafael_Veiga);
